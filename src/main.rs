f35ba82fd83b00ae589584530a4bb77536cf5caf
dustin4242 <thegameexposer@gmail.com>
2023-04-21 19:47:18 -0500

    whoops wrong type

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ fn main() -> Result<(), Error> {
                     }
                 }
                 input = String::new();
-                term.write_all((current_directory.as_str() + "> ").as_bytes())?;
+                term.write_all((current_directory.to_string() + "> ").as_bytes())?;
             }
             _ => (),
         };
